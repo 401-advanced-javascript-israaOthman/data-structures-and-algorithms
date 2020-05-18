1de753277e3b404cc885135e3db9afd07f1577cf
israaothman <israaayoub@hotmail.com>
2020-05-18 18:18:26 +0300

    done-02

---
+++ code-challenges/arrayShift/array-shift.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function insertShiftArray(arr,num){
+    let ln = Math.ceil( arr.length/2);
+    let copy = [];
+for(let i=0 ; i<arr.length+1;i++){
+   if(i === ln){
+     copy[i] = num;
+   } else if (i>ln){
+     copy[i] = arr[i-1];
+   }else{
+   copy[i] = arr[i];}
+}   
+
+return copy;
+} 
+
+let arr1=[2,4,6,8];
+let arr2 = [4,8,15,23,42];
+
+console.log(insertShiftArray(arr1,5));
+console.log(insertShiftArray(arr2,16));
+console.log(insertShiftArray([],16));
+
