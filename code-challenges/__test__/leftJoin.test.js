b440a81df811087f9436342d4149542364ca5b87
israaothman <israaayoub@hotmail.com>
2020-07-08 23:59:33 +0300

    done cc33

---
+++ code-challenges/__test__/leftJoin.test.js
@@ -0,0 +1,27 @@
+// 'use strict';
+
+// const Hashmap = require('../../hashtable/hashtable');
+// const leftJoin = require('../../leftJoin/left-join');
+
+// let hash1 = new Hashmap(1024);
+// let hash2 = new Hashmap(1024);
+
+// hash1.set('test','synonym');
+// // hash1.set('testt','synonymm');
+// // hash1.set('testtt','synonymmm');
+
+// hash2.set('test','antonyms');
+// // hash2.set('testt','antonymss');
+// // hash2.set('testtt','antonymsss');
+ 
+// // console.log('hash11111111111',hash1);
+// // console.log('hash22222222222',hash2);
+
+// describe('left join',()=>{
+  
+//   it('left join',()=>{
+
+//     expect(leftJoin(hash1,hash2)).toEqual([['test','synonym','antonyms']]);
+//   });
+// });
+  
--- leftJoin/left-join.js
+++ leftJoin/left-join.js
@@ -1,2 +1,34 @@
 'use strict';
 
+
+function leftJoin (hash1,hash2){
+  let map1 = hash1.map;
+  let result=[];
+  map1.forEach(lL=>{
+    //   console.log(lL);
+    let values = lL.values();
+    // console.log(values);
+    values.forEach(object=>{
+      let smallArr = [];
+      if(Object.keys(object)[0]){
+        // console.log(object);
+        // console.log()
+        let test=Object.keys(object)[0]
+
+        smallArr[0]=test;
+        smallArr[1]=object[test];
+        // console.log(smallArr);
+        let antonyms= hash2.get(test);
+        console.log(antonyms);
+        if(antonyms){
+          smallArr[2]=antonyms[test];
+        }
+      }
+      result.push(smallArr);
+    });
+  });
+  return result;
+}
+
+
+module.exports=leftJoin;
